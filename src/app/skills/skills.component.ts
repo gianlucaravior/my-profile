d7adf73cd52499126e6fe351e7a6b2ec1b7d5a2a
Gianluca <84138648+gianlucaravior@users.noreply.github.com>
2024-03-31 15:16:57 +0200

    align left logos safari, add robots and sitemap, fix footer margin

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { createPopup } from "@typeform/embed";
+
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
-
+  openTypeform() {
+    createPopup("MVw1EbOi", { size: 90 }).open(); // call open() on created popup
+  }
 }
